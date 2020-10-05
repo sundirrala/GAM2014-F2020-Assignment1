06de421e883dad797be4ccad4750025343a055b6
sundirrala <nisara.drahman@gmail.com>
2020-10-04 22:36:11 -0400

    initial commit

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,50 @@
+﻿// summary
+// Nisara Drahman
+// 101120917
+// oct 03 2020, button behaviour script
+// this is where i put in the functionality for every button in game.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayButtonPressed()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnHowToPlayButtonPressed()
+    {
+        SceneManager.LoadScene("How To Play");
+    }
+
+    public void OnMainMenuPressed()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OnGameOverPressed()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void OnBackButtonPressed()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
